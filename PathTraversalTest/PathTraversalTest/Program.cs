9cdeb6d3b1e2091295b3e20b6e39f6020ffb4ebc
Jon Bradford <548416+jbradford@users.noreply.github.com>
2025-04-01 10:28:19 +0100

    Added path traversal test project

---
+++ PathTraversalTest/PathTraversalTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var filePath = Console.ReadLine();
+if (filePath != null)
+{
+    var file = File.ReadAllText(filePath);
+    Console.WriteLine(file);
+}
